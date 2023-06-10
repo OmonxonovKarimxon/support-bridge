4671fc05d3974a7145638042dee81f6bc86850ae
Suxrob-Jurakulov <suxrobbek997@gmail.com>
2023-06-10 13:06:13 +0500

    some changes

--- question-service/src/main/java/com/example/QuestionServiceApplication.java
+++ question-service/src/main/java/com/example/QuestionServiceApplication.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
 @EnableDiscoveryClient
+@SpringBootApplication
 public class QuestionServiceApplication {
 
     public static void main(String[] args) {
---
+++ question-service/src/main/java/com/example/proxy/AuthProxy.java
@@ -0,0 +1,17 @@
+package com.example.proxy;
+
+import com.example.dto.UserAccountDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+
+@FeignClient(name = "auth-server")
+public interface AuthProxy {
+
+    @GetMapping("/profile")
+    ResponseEntity<UserAccountDto> getUserProfile(@RequestHeader(value = "Authorization") String authorizationHeader);
+
+}
+
